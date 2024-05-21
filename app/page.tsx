9f7878b83e750ea69edc2383813567eb0d1e3f9c
UsboKirishima <usbertibox@gmail.com>
2024-05-21 16:06:17 +0200

    fix(*): add avatars gifs to TeamMembers

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ interface TeamMembers {
   name: string;
   surname: string;
   role: string;
+  iconURL: string;
 }
 
 
@@ -17,136 +18,163 @@ const teamMembers: TeamMembers[] = [
     name: "DAVE BILL",
     surname: "AGBEKORNU",
     role: 'Team Member',
+    iconURL: 'https://cdn3.f-cdn.com//files/download/152864535/Discord%20Emoji%20GIFs%20Animations%20%283%29.gif?fit=crop',
   },
   {
     name: "ROBERTO",
     surname: "AGNETTI",
     role: 'Team Member',
+    iconURL: 'https://cdn3.f-cdn.com//files/download/152864535/Discord%20Emoji%20GIFs%20Animations%20%283%29.gif?fit=crop',
   },
   {
     name: "GLORIA",
     surname: "ARCURI",
     role: 'Team Member',
+    iconURL: 'https://cdn3.f-cdn.com//files/download/152864535/Discord%20Emoji%20GIFs%20Animations%20%283%29.gif?fit=crop',
   },
   {
     name: "MYKHAYLO",
     surname: "ASHYFIN",
     role: 'Team Member',
+    iconURL: 'https://lh3.googleusercontent.com/s-VELe57Ni9VT1Md5_mJavzm93w2fa97y_BWChha_JJxaIOzD0R8VHejUIcsWUINVn18aGU0YOZyPmQQfqeecQiMjfWdcrAPMxzjcw',
   },
   {
     name: "RICCARDO",
     surname: "BARBACINI",
     role: 'Team Member',
+    iconURL: 'https://cdn3.f-cdn.com//files/download/152864535/Discord%20Emoji%20GIFs%20Animations%20%283%29.gif?fit=crop',
   },
   {
     name: "ADAM",
     surname: "BEN HOUSSINE",
     role: 'Team Member',
+    iconURL: 'https://lh3.googleusercontent.com/oHzPEgsQJgVW1CB0EMsfVpJ556SzRPlAQUAhybYuYjQEeTRmLh-DtkqxCmbd1ccciAp-IgAlHVR1mSHrPNa4_HO5b9g6P8HDm7k=s400',
   },
   {
     name: "LORENZO",
     surname: "BIGNOTTI",
     role: 'Team Member',
+    iconURL: 'https://lh3.googleusercontent.com/ZN7xOyL6Zf3LcaavLscWP7UHdv-lm_2ULsU3h5VdMzOK5Lk-prQWVMIyRVmHD0dI_nIXA8b9r9gI1XvNEj2tBeWhadJo0-fyje0=s400',
   },
   {
     name: "LEONARDO",
     surname: "CAVALCA",
     role: 'Team Member',
+    iconURL: 'https://cdn3.f-cdn.com//files/download/152864535/Discord%20Emoji%20GIFs%20Animations%20%283%29.gif?fit=crop',
   },
   {
     name: "LUCA",
     surname: "COPPELLOTTI",
     role: 'Team Member',
+    iconURL: 'https://cdn3.f-cdn.com//files/download/152864535/Discord%20Emoji%20GIFs%20Animations%20%283%29.gif?fit=crop',
   },
   {
     name: "DARIO",
     surname: "DI SALVATORE",
-    role: 'Team Member',
+    role: 'Marketing Manager',
+    iconURL: 'https://i.redd.it/i6x6h197ixg81.gif',
   },
   {
     name: "OMAR",
     surname: "DJEBALI",
     role: 'Team Member',
+    iconURL: 'https://cdn3.f-cdn.com//files/download/152864535/Discord%20Emoji%20GIFs%20Animations%20%283%29.gif?fit=crop',
   },
   {
     name: "DIEGO",
     surname: "ESPOSITO",
-    role: 'Team Member',
+    role: 'Co-Founder',
+    iconURL: 'https://openseauserdata.com/files/f1ad65d5132e836c0442cf7efb546277.gif',
   },
   {
     name: "MATTEO",
     surname: "FORNACIARI",
     role: 'Team Member',
+    iconURL: 'https://res.cloudinary.com/coin-nft/cache/1/e8/96/e89679d2ab1db9091de9c8bcb7b13464c4f4a60e6a3ef13e419dadc4400b8dd2-YWIzOTRlOGUtZWEyMy00YmY2LWE2MGYtZjE2MTQzZjlhZDc3',
   },
   {
     name: "MATTEO",
     surname: "FRANCAVILLA",
     role: 'Team Member',
+    iconURL: 'https://cdn3.f-cdn.com//files/download/152864535/Discord%20Emoji%20GIFs%20Animations%20%283%29.gif?fit=crop',
   },
   {
     name: "SALVATORE",
     surname: "FRATTARUOLO",
     role: 'Team Member',
+    iconURL: 'https://cdn3.f-cdn.com//files/download/152864535/Discord%20Emoji%20GIFs%20Animations%20%283%29.gif?fit=crop',
   },
   {
     name: "MATTHEW",
     surname: "GASPARETTI",
-    role: 'Team Member',
+    role: 'Founder',
+    iconURL: 'https://lh3.googleusercontent.com/p6AT4gZ2Grq2N4Y7YSWIaGI7WXMJb7swSYMpfggs0DGYglywXfwULZZo1hJmH4UPj92AMOgcO-0CgRLABtcCYsgd7L6zBPREjiIeAYU',
   },
   {
     name: "RICCARDO",
     surname: "GRANELLI",
     role: 'Team Member',
+    iconURL: 'https://cdn3.f-cdn.com//files/download/152864535/Discord%20Emoji%20GIFs%20Animations%20%283%29.gif?fit=crop',
   },
   {
     name: "ADAM",
     surname: "IZEM",
-    role: 'Team Member',
+    role: 'RSPP & RSL',
+    iconURL: 'https://cdn.discordapp.com/attachments/1165008819391299769/1242382427343687700/download.gif?ex=664da24d&is=664c50cd&hm=7dc8f18b8f28111f08181d500394d85ee94ec6c74160bcda576e24f3a259f187&',
   },
   {
     name: "NICOLO",
     surname: "LAMANUZZI",
     role: 'Team Member',
+    iconURL: 'https://lh3.googleusercontent.com/NA-9sudNHE5E3ra5n56tDddAlJ9AhR8KKf5HQ2Hupmlt0Bwijw_omAy2JLbg3gWudojAHa8RAzgrH9rAfTC7Ozf7tgJvMJpaYV8=s400',
   },
   {
     name: "TOMMASO",
     surname: "PREDIERI",
     role: 'Team Member',
+    iconURL: 'https://gifsec.com/wp-content/uploads/2022/12/nft-gif-25.gif',
   },
   {
     name: "MATTIA PIO",
     surname: "RIZZO",
     role: 'Team Member',
+    iconURL: 'https://assets.raribleuserdata.com/prod/v1/image/t_avatar_big/aHR0cHM6Ly9pcGZzLnJhcmlibGV1c2VyZGF0YS5jb20vaXBmcy9RbVd4REdNc3htTHVyU0JBbmVNakVNamJLQWJpUHVzVGNpcU1Sem9BYUhIcXFF',
   },
   {
     name: "MATTIA",
     surname: "SOZZI",
     role: 'Team Member',
+    iconURL: 'https://cdn3.f-cdn.com//files/download/152864535/Discord%20Emoji%20GIFs%20Animations%20%283%29.gif?fit=crop',
   },
   {
     name: "COSTANTINO",
     surname: "TATARU",
     role: 'Team Member',
+    iconURL: 'https://cdn3.f-cdn.com//files/download/152864535/Discord%20Emoji%20GIFs%20Animations%20%283%29.gif?fit=crop',
   },
   {
     name: "IAROSLAV",
     surname: "TOFAN",
     role: 'Team Member',
+    iconURL: 'https://cdn3.f-cdn.com//files/download/152864535/Discord%20Emoji%20GIFs%20Animations%20%283%29.gif?fit=crop',
   },
   {
     name: "DAVIDE",
     surname: "USBERTI",
-    role: 'Team Member',
+    role: 'Website Developer',
+    iconURL: 'https://openseauserdata.com/files/11ff34e57a4e56e65cd9442adf4fe217.gif',
   },
   {
     name: "PIETRO",
     surname: "ZACCARINI",
     role: 'Team Member',
+    iconURL: 'https://lh3.googleusercontent.com/pIqonSnzV8tB82E2J6ecPCa5rREVxPMfHWefRPMo3asIJa21ffqqpR6MN2NQLQnOgNeCBEK2k7XpOmcCZrIfcItpxZ3SLv-BRcw=s400',
   },
   {
     name: "MARIA",
     surname: "ZEKO",
     role: 'Team Member',
+    iconURL: 'https://cdn3.f-cdn.com//files/download/152864535/Discord%20Emoji%20GIFs%20Animations%20%283%29.gif?fit=crop',
   }
 ]
 
@@ -256,6 +284,7 @@ export default function Home() {
                   name={member.name}
                   surname={member.surname}
                   role={member.role}
+                  iconURL={member.iconURL}
                   key={index}
                 />
               ))}
--- components/ProfileCard.tsx
+++ components/ProfileCard.tsx
@@ -1,9 +1,16 @@
 import Image from "next/image";
 
-const ProfileCard = (props: any) => {
+interface MemberProp {
+    name: string;
+    surname: string;
+    role: string;
+    iconURL: string;
+  }
+
+const ProfileCard = (props: MemberProp) => {
     return (
         <div className="bg-[#393552]/20 shadow-md rounded-xl mb-8 p-4 hover:scale-105 transition  duration-300 hover:bg-[#393552]/30 hover:transition hover:duration-300">
-            <img src="https://cdn3.f-cdn.com//files/download/152864535/Discord%20Emoji%20GIFs%20Animations%20%283%29.gif?fit=crop"
+            <img src={props.iconURL}
                 className=" max-w-[100px] mx-auto mb-4 rounded-full shadow-lg dark:shadow-black/20" alt="" />
 
             <p className="mb-2 font-bold text-[#6e6a86]">{props.name} {props.surname}</p>
